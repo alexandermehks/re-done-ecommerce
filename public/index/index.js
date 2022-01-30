0a6710ad5e0b1eca7fad73afa28d55bc050cd0e0
iiamalex <alexandermehks@gmail.com>
2022-01-30 16:48:14 +0100

    småfix

---
+++ public/index/index.js
@@ -0,0 +1,20 @@
+const vm = new Vue({
+     el: "#app",
+     data: {
+
+     },
+
+     methods: {
+          do_test() {
+               console.log("From index.js")
+          }
+     }
+
+
+
+
+})
+
+
+frontend / index.js
+     / home / am / git / kingnation / frontend / index.js
