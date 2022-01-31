7eebae838a68b2a0087eda8e111f19fa61f9502a
iiamalex <alexandermehks@gmail.com>
2022-01-31 16:28:23 +0100

    Databas skit

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -1,1 +1,20 @@
+const routes = require('express').Router();
+const dbService = require('../database/db_products')
+const { request } = require('http');
+const { response } = require('express');
+
+
+
+routes.get('/all', async (req, res) => {
+     try {
+          const users = await dbService.getProducts();
+          res.send(users);
+     }
+     catch (error) {
+          res.sendStatus(400, "Something went wrong");
+     }
+});
+
+
+module.exports = routes;
 
