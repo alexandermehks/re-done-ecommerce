80e93551ce5d030015a691dc5146ab5a8b1c0e95
rockfarmor <johan.nilsson1227@gmail.com>
2022-01-31 11:53:26 +0100

    Added shit

--- public/index/index.js
+++ public/index/index.js
@@ -1,34 +1,42 @@
 const vm = new Vue({
-     el: "#app",
-     data: {
+    el: "#app",
+    data: {
+        msg: 'aasd',
+        msg2: 'asd2',
+        title: "Alex webpage",
+        posts: [
+            { id: 1, title: 'My journey with Vue' },
+            { id: 2, title: 'Blogging with Vue' },
+            { id: 3, title: 'Why Vue is so fun' },
+            { id: 3, title: 'HELO ALEX' }
+        ]
+    },
 
-     },
+    methods: {
+        do_test() {
+            alert("Function from index.js VUE WORKS")
+            console.log("From index.js")
+            document.title = "A new title"
+        },
 
-     methods: {
-          do_test() {
-               alert("Function from index.js VUE WORKS")
-               console.log("From index.js")
-          },
-
-          getUser() {
-               $.ajax({
-                    url: '/user/users',
-                    type: 'GET',
-                    success: (result) => {
-                         const element = document.getElementById("FAK")
-                         if (element.style.color === "red") {
-                              element.setAttribute("style", "color:black");
-                         }
-                         else {
-                              element.setAttribute("style", "color:red");
-                         }
-                         console.log(result[0].name)
-
+        getUser() {
+            $.ajax({
+                url: '/user/users',
+                type: 'GET',
+                success: (result) => {
+                    const element = document.getElementById("FAK")
+                    if (element.style.color === "red") {
+                        element.setAttribute("style", "color:black");
+                    } else {
+                        element.setAttribute("style", "color:red");
                     }
-               })
+                    console.log(result[0].name)
 
-          }
-     }
+                }
+            })
+
+        }
+    }
 
 
 
@@ -36,5 +44,5 @@ const vm = new Vue({
 })
 
 
-frontend / index.js
-     / home / am / git / kingnation / frontend / index.js
+frontend / index.js /
+    home / am / git / kingnation / frontend / index.js
