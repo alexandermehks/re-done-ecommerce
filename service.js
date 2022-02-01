3c96c09eb2d3a76472aa8c52b8064cc3b654469c
Sebastian <bassenilsson93@gmail.com>
2022-02-01 10:18:59 +0100

    asd
    asd

--- service.js
+++ service.js
@@ -8,6 +8,7 @@ const app = express();
 const port = 3000;
 
 const user_routes = require('./routes/user_routes')
+const prod_routes = require('./routes/product_routes')
 
 //Setting statick folder and removing the .html in the end of the file. 
 app.use(express.static(path.join(__dirname, 'public/index'), { extensions: ['html'] }));
@@ -18,6 +19,7 @@ app.use(express.json());
 
 //Default routing for user routes will be /user
 app.use('/user', user_routes);
+app.use('/products', prod_routes);
 
 //app.get('/', function (req, res) {
 //res.send('HomePage')
@@ -28,5 +30,5 @@ app.use('/user', user_routes);
  * Listening to port!
  */
 app.listen(port, () => {
-    console.log('Now listening on port ' + port);
+     console.log('Now listening on port ' + port);
 });
