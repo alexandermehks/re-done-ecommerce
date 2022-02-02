eda72ea3574f5a2f1aea24765ad6fc00bb1baf16
rockfarmor <johan.nilsson1227@gmail.com>
2022-02-02 16:03:25 +0100

    Added product route /all-johan that generates some fake data so we can start implemetning products

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -5,41 +5,54 @@ const { response } = require('express');
 
 
 
-routes.get('/all', async (req, res) => {
-     try {
-          const users = await dbService.getProducts();
-          res.send(users);
-     }
-     catch (error) {
-          res.sendStatus(400, "Something went wrong");
-     }
+routes.get('/all', async(req, res) => {
+    try {
+        const users = await dbService.getProducts();
+        res.send(users);
+    } catch (error) {
+        res.sendStatus(400, "Something went wrong");
+    }
+});
+
+routes.get('/all-johan', async(req, res) => {
+    try {
+        const users = await dbService.getProductsJohan();
+
+
+        res.send(users);
+    } catch (error) {
+        res.sendStatus(400, "Something went wrong");
+    }
 });
 
 
-routes.post('/add', async (req, res) => {
-     try {
-          let mes = await dbService.addProduct(req.body);
-          if (mes) {
-               res.send("Success")
-          }
 
-     }
-     catch (error) {
-          res.sendStatus(400, "Something went wrong");
-     }
+
+
+
+
+
+routes.post('/add', async(req, res) => {
+    try {
+        let mes = await dbService.addProduct(req.body);
+        if (mes) {
+            res.send("Success")
+        }
+
+    } catch (error) {
+        res.sendStatus(400, "Something went wrong");
+    }
 })
 
 
-routes.get('/:category', async (req, res) => {
-     try {
-          const answer = await dbService.getCategory(req.params.category);
-          res.send(answer);
-     }
-     catch (error) {
-          res.sendStatus(400, "Something went wrong");
-     }
+routes.get('/:category', async(req, res) => {
+    try {
+        const answer = await dbService.getCategory(req.params.category);
+        res.send(answer);
+    } catch (error) {
+        res.sendStatus(400, "Something went wrong");
+    }
 })
 
 
 module.exports = routes;
-
