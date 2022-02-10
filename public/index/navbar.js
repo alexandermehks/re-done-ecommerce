dbd5c97d5aa8f847a26d6292be5bd82e2b51eec3
iiamalex <alexandermehks@gmail.com>
2022-02-10 16:43:39 +0100

    Google login V.01

--- public/index/navbar.js
+++ public/index/navbar.js
@@ -52,12 +52,16 @@ const navbarvm = new Vue({
                          location.reload();
                     }
                })
-          }
+          },
+
+
 
      },
 
 
      mounted() {
+
+
 
 
 
@@ -75,6 +79,37 @@ const navbarvm = new Vue({
 });
 
 navbarvm.getLoggedInUser();
+
+
+
+function onSignIn(googleUser) {
+     let user = {}
+     user['status'] = true;
+     var profile = googleUser.getBasicProfile();
+     user['name'] = profile.getName();
+     //user['image'] = profile.getImageUrl();
+     user['email'] = profile.getEmail();
+     user['type'] = "GOOGLE";
+     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+     console.log('Name: ' + profile.getName());
+     console.log('Image URL: ' + profile.getImageUrl());
+     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+     navbarvm.loggedin = user;
+}
+
+
+function signOut() {
+     var auth2 = gapi.auth2.getAuthInstance();
+     auth2.signOut().then(function () {
+          console.log('User signed out.');
+     });
+     location.reload();
+}
+
+
+
+
+
 
 
 
