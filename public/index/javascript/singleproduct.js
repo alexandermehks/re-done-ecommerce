4647ece0187c1f5fdc692fa823cb569f65e31ee8
iiamalex <alexandermehks@gmail.com>
2022-02-11 11:02:36 +0100

    Sebbe KUNG push

--- public/index/javascript/singleproduct.js
+++ public/index/javascript/singleproduct.js
@@ -1,74 +1,158 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-  $("#reviewbutton").click(
-    function() {
-        $(".reviewform").slideToggle();
+     $("#reviewbutton").click(
+          function () {
+               $(".reviewform").slideToggle();
 
-    });
- 
+          });
+
 
 
 
 });
 
-function emptysizes()
-{
-    document.getElementById('S').setAttribute("class", "style1");
+function emptysizes() {
+     document.getElementById('S').setAttribute("class", "style1");
 }
-function changeStarColor(id)
-{
-  
-  
-  
-  document.getElementById(id).style.color = "orange";
-  
- 
+
+function changeStarColor(id) {
+     document.getElementById(id).style.color = "orange";
+}
 
 
-  
- 
-  }
 
 
-  function changeColor(id)
-{
-  
-  console.log(id)
-  
-  document.getElementById("S").style.backgroundColor = "white";
-  document.getElementById("M").style.backgroundColor = "white";
-  document.getElementById("L").style.backgroundColor = "white";
-  document.getElementById("XL").style.backgroundColor = "white";
-  document.getElementById("2XL").style.backgroundColor = "white"; // backcolor
-  document.getElementById(id).style.backgroundColor = "lightgray"; // backcolor
-  var x = document.getElementById("sizehelpS");
-  x.style.display === "none"
-  if (id === "S"){
-    document.getElementById("length").innerHTML = "172-184 cm";
-    document.getElementById("weight").innerHTML = "70-74 kg";
-    document.getElementById("chest").innerHTML = "95-99 cm";
 
-  }else if(id === "M"){
-    document.getElementById("length").innerHTML = "174-188 cm";
-    document.getElementById("weight").innerHTML = "75-84 kg";
-    document.getElementById("chest").innerHTML = "100-104 cm";
-  }else if(id === "L"){
-    document.getElementById("length").innerHTML = "176-192 cm";
-    document.getElementById("weight").innerHTML = "85-94 kg";
-    document.getElementById("chest").innerHTML = "105-109 cm";
-  }else if(id === "XL"){
-    document.getElementById("length").innerHTML = "178-196 cm";
-    document.getElementById("weight").innerHTML = "95-104 kg";
-    document.getElementById("chest").innerHTML = "110-114 cm";
-  }else if(id === "2XL"){
-    document.getElementById("length").innerHTML = "180-196 cm";
-    document.getElementById("weight").innerHTML = "105-109 kg";
-    document.getElementById("chest").innerHTML = "115-119 cm";
-  }
-  x.style.display = "block"
-  let text = "Size "+id+" fits:";
-  document.getElementById("sizefit").innerHTML = text.bold();
-  
- 
-  }
+$(function () {
+     $("#navbar").load("navbar.html");
+});
+
+const vm = new Vue({
+
+     el: "#app",
+     data: {
+          product: [],
+          prodname: "",
+          sizes: [],
+          order: [{
+               "prodID": "None",
+               "propID": "None",
+
+
+          }],
+     },
+
+     methods: {
+          loadProduct(id) {
+               //Load  questions
+               $.ajax({
+                    url: 'products/byProdId/' + id,
+                    type: 'GET',
+                    success: (data) => {
+                         this.product = data
+                         this.prodname = data[0].name
+                         this.order.prodID = data[0].prodID
+
+                         for (var i = 0; i < data.length; i++) {
+
+                              if (data[i].balance === data[0].balance) {
+                                   if (jQuery.inArray(data[i].size, this.sizes) != -1) {
+
+                                   } else {
+                                        this.sizes.push(data[i].size)
+                                   }
+
+                              }
+                         }
+
+                         this.sizes.sort();
+
+
+                    },
+                    error: (data) => {
+                         console.log("AAA")
+                         return null
+                    }
+               });
+
+               //update questions
+          },
+
+
+
+          updatePicturePick: function (id) {
+
+               $('#' + id).css({
+                    'border': '1px solid black'
+               });
+
+          },
+          updateSizePick: function (id, products) {
+               for (var i = 0; i < products.length; i++) {
+                    $('#' + products[i].propID + "1").css({
+                         'background-color': 'white'
+                    });
+               }
+               $('#' + id + "1").css({
+                    'background-color': 'lightgrey'
+               });
+
+
+          },
+          updatePicture: function (url) {
+               $("#picbig").attr('src', url);
+
+
+          },
+          updatePictureColor: function (url) {
+               $("#picbig").attr('src', url);
+               for (var i = 0; i < this.product.length; i++) {
+                    console.log(this.product[i].size)
+                    $('#' + this.product[i].size).css({
+                         'background-color': 'white'
+                    });
+
+               }
+
+          },
+          changeColor: function (id) {
+               for (var i = 0; i < this.product.length; i++) {
+                    console.log(this.product[i].size)
+                    $('#' + this.product[i].size).css({
+                         'background-color': 'white'
+                    });
+
+               }
+               $('#' + id).css({
+                    'background-color': 'lightgrey'
+               });
+
+          },
+          updateCorrectSizes: function (colorID) {
+               this.sizes = []
+
+
+               for (var i = 0; i < this.product.length; i++) {
+                    if (this.product[i].balance === colorID) {
+                         if (jQuery.inArray(this.product[i].size, this.sizes)) {
+                              this.sizes.push(this.product[i].size)
+                         } else {
+                              this.sizes.push(this.product[i].size)
+                         }
+
+                    }
+               }
+
+               this.sizes.sort();
+               $("#sizecontainer").load(window.location.href + " #sizecontainer");
+
+          },
+
+     }
+
+
+
+});
+vm.loadProduct("id1")
+
