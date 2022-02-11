d9d95403e952062ac68ffc7d7bd16a28a6cd3158
iiamalex <alexandermehks@gmail.com>
2022-02-11 16:40:05 +0100

    Add pictures for prodcuts v.0.1

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -6,37 +6,37 @@ const fs = require('fs');
 
 
 
-routes.get('/all', async(req, res) => {
-    try {
-        const users = await dbService.getProducts();
-        res.send(users);
-    } catch (error) {
-        res.sendStatus(400, "Something went wrong");
-    }
+routes.get('/all', async (req, res) => {
+     try {
+          const users = await dbService.getProducts();
+          res.send(users);
+     } catch (error) {
+          res.sendStatus(400, "Something went wrong");
+     }
 });
 
-routes.get('/allOnlyProduct', async(req, res) => {
-    try {
-        const users = await dbService.getOnlyProducts();
-        res.send(users);
-    } catch (error) {
-        res.sendStatus(400, "Something went wrong");
-    }
+routes.get('/allOnlyProduct', async (req, res) => {
+     try {
+          const users = await dbService.getOnlyProducts();
+          res.send(users);
+     } catch (error) {
+          res.sendStatus(400, "Something went wrong");
+     }
 });
 
-routes.get('/byProdId/:id', async(req, res) => {
-    try {
+routes.get('/byProdId/:id', async (req, res) => {
+     try {
 
-        if (!req.params.id)
-            return res.send("please provide an id");
+          if (!req.params.id)
+               return res.send("please provide an id");
 
-        const users = await dbService.getProductsByProdID(req.params.id);
-        return res.send(users);
+          const users = await dbService.getProductsByProdID(req.params.id);
+          return res.send(users);
 
-    } catch (error) {
-        console.log(error)
-        return res.sendStatus(400, "Something went wrong");
-    }
+     } catch (error) {
+          console.log(error)
+          return res.sendStatus(400, "Something went wrong");
+     }
 });
 
 
@@ -45,92 +45,95 @@ routes.get('/byProdId/:id', async(req, res) => {
 
 
 
-routes.post('/add', async(req, res) => {
-    try {
-        let mes = await dbService.addProduct(req.body);
-        if (mes) {
-            res.send("Success")
-        }
+routes.post('/add', async (req, res) => {
+     try {
+          console.log(req.body)
+          let mes = await dbService.addProduct(req.body);
+          if (mes) {
+               const prodID = mes;
+               console.log(mes, "messenge")
+               res.send(prodID)
+          }
 
-    } catch (error) {
-        res.sendStatus(400, "Something went wrong");
-    }
+     } catch (error) {
+          res.sendStatus(400, "Something went wrong");
+     }
 })
 
-routes.put('/edit', async(req, res) => {
-    try {
-        console.log(req.body, "WE OUT BAD")
-        let mes = await dbService.editProduct(req.body);
+routes.put('/edit', async (req, res) => {
+     try {
+          console.log(req.body, "WE OUT BAD")
+          let mes = await dbService.editProduct(req.body);
 
-        if (mes) {
-            res.send("Success")
-        }
+          if (mes) {
+               res.send("Success")
+          }
 
-    } catch (error) {
-        res.sendStatus(400, "Something went wrong");
-    }
+     } catch (error) {
+          res.sendStatus(400, "Something went wrong");
+     }
 })
 
-routes.post('/addProperty', async(req, res) => {
-    try {
-        let mes = await dbService.addProductProperty(req.body);
-        if (mes) {
-            res.send("Success")
-        }
+routes.post('/addProperty', async (req, res) => {
+     try {
+          let mes = await dbService.addProductProperty(req.body);
+          if (mes) {
+               res.send("Success")
+          }
 
-    } catch (error) {
-        res.sendStatus(400, "Something went wrong");
-    }
+     } catch (error) {
+          res.sendStatus(400, "Something went wrong");
+     }
 })
 
 
 
-routes.post('/uploadpicture', async(req, res) => {
-    try {
-        if (req.files) {
-            fs.mkdir(`./uploads/products/${req.body.text}`, (err) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log("SUCESS")
-                }
-            })
-
-            if (!req.files.length) {
-                console.log("HÄR")
-            }
-            for (let i = 0; i < req.files.file.length; i++) {
-                var file = req.files.file[i]
-                var filename = file.name
-                const p = `./uploads/products/${req.body.text}/`
-                file.mv(p + filename, function(err) {
+routes.post('/uploadpicture', async (req, res) => {
+     try {
+          if (req.files) {
+               fs.mkdir(`./uploads/products/${req.body.id}`, (err) => {
                     if (err) {
-                        res.send("Upload failed");
+                         console.log(err)
+                    } else {
+                         console.log("SUCESS")
                     }
-                });
-                const query = `products/${req.body.text}/${filename}`
-                const addtoDatabase = await dbService.addPicture(req.body.text, query)
-            }
-            res.send("OK")
-        } else {
-            res.send("ERROR")
-        }
+               })
+
+               if (!req.files.length) {
+                    console.log("HÄR")
+               }
+               for (let i = 0; i < req.files.file.length; i++) {
+                    var file = req.files.file[i]
+                    var filename = file.name
+                    const p = `./uploads/products/${req.body.id}/`
+                    file.mv(p + filename, function (err) {
+                         if (err) {
+                              res.send("Upload failed");
+                         }
+                    });
+                    const query = `products/${req.body.id}/${filename}`
+                    const addtoDatabase = await dbService.addPicture(req.body.id, query)
+               }
+               res.send("OK")
+          } else {
+               res.send("ERROR")
+          }
 
 
-    } catch (error) {
-        res.sendStatus(400, "Something went wrong");
-    }
+     } catch (error) {
+          res.sendStatus(400, "Something went wrong");
+     }
 });
 
 
 
-routes.get('/pictures/:id', async(req, res) => {
-    try {
-        const answer = await dbService.getPicture(req.params.id);
-        res.send(answer)
-    } catch (error) {
-        res.sendStatus(400, "something went wrong")
-    }
+routes.get('/pictures/:id', async (req, res) => {
+     try {
+          const answer = await dbService.getPicture(req.params.id);
+          res.send(answer)
+     } catch (error) {
+          res.sendStatus(400, "something went wrong")
+     }
 })
 
 
