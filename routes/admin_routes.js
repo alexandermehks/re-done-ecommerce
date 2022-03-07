d2cdf1a6bb4c8177f0fdf062176bf622508e187f
Kingen8 <h19tobla@du.se>
2022-03-07 11:16:00 +0100

    backend admin routes

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,1 +1,53 @@
+const routes = require('express').Router();
+const dbService = require('../database/db_user')
+const { request } = require('http');
+const { response } = require('express');
+const res = require('express/lib/response');
+const bcrypt = require('bcrypt');
+const { nextTick } = require('process');
+const session = require('express-session');
 
+
+/**
+ * Route to send the get request to when you want all the users.
+ */
+routes.get('/users', async(req, res) => {
+    try {
+        const user = await dbService.getUsers();
+        res.send(user);
+    } catch (error) {
+        res.sendStatus(400, "Something went wrong");
+    }
+});
+
+routes.put('/editUser', async(req, res) => {
+    try {
+
+        let user = await dbService.updateUser(req.body);
+
+        if (user) {
+            res.send("Success")
+        }
+
+    } catch (error) {
+        res.sendStatus(400, "Something went wrong");
+    }
+
+})
+
+routes.post('/deleteUser', async(req, res) => {
+    try {
+
+        let del = await dbService.deleteUsers(req.body);
+
+
+        if (del) {
+            res.send("SUCCESS")
+        }
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(400, "Something went wrong");
+    }
+
+})
+module.exports = routes;
