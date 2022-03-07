1aacaa398ae38f540e014b743536916615e1052e
Sebastian <bassenilsson93@gmail.com>
2022-03-07 11:06:25 +0100

    awd
    awd

--- public/index/javascript/adminManagePage.js
+++ public/index/javascript/adminManagePage.js
@@ -6,7 +6,7 @@ const vm = new Vue({
     },
 
     mounted() {
-        $(function() {
+        $(function () {
             $("#navbar").load("navbar.html");
             $("#footer").load("footer.html");
         });
@@ -14,29 +14,49 @@ const vm = new Vue({
 
     },
     methods: {
-        changeManage: function() {
-            $("#accountinfo").toggle();
-            $("#orders").toggle();
+        changeManage: function () {
+            var element = document.getElementById("accountinfo");
+            var element1 = document.getElementById("orders");
+            if (element) {
+                var display = element.style.display;
+                if (display == "none") {
+                    element.style.display = "block";
+                    element1.style.display = "none";
+                }
+            }
+        },
+        changeManage2: function () {
+            var element = document.getElementById("orders");
+            var element1 = document.getElementById("accountinfo");
+            if (element) {
+                var display = element.style.display;
+                if (display == "none") {
+                    element.style.display = "block";
+                    element1.style.display = "none";
+
+                }
+            }
+
 
         },
-        toggleEditAccount: function() {
+        toggleEditAccount: function () {
             $("#editAccount").toggle();
             $("#editAccountSubmit").toggle();
 
-            
+
         },
 
-        updateAccount: function() {
+        updateAccount: function () {
             var username = document.getElementById("username").value;
             var email = document.getElementById("email").value;
             console.log(username)
             console.log(email)
 
 
-            
+
         }
 
-       
+
     }
 
 });
