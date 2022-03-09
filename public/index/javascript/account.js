2bf17f13ce3af45199bdc421ce6d6c72a812bb35
Luca2593 <98734589+Luca2593@users.noreply.github.com>
2022-03-09 12:51:33 +0100

    account page
    account page and js

--- public/index/javascript/account.js
+++ public/index/javascript/account.js
@@ -1,7 +1,7 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        testAccount: "luca123"
+        loggedin:{},
 
     },
 
@@ -10,14 +10,61 @@ const vm = new Vue({
             $("#navbar").load("navbar.html");
             $("#footer").load("footer.html");
         });
+        this.getLoggedInUser();
 
 
     },
     methods: {
-        do_test() {
-            console.log("test")
+        getLoggedInUser() {
+            $.ajax({
+                url: '/auth/loggedInUser',
+                type: 'GET',
+                success: (result) => {
+                    this.loggedin = result;
+                    console.log(result);
+                    console.log(result.id);
+                    console.log(result.name);
+                    console.log(result.email);
+                    console.log(result.password); 
+                }
+            })
         },
+        updateEmail() {
+            const user = {
 
+                "id": this.loggedin.id,
+                "name": this.loggedin.name,
+                "email": $('#InputEmail').val(),
+                
+
+            }
+            $.ajax({
+                url: '/admin/editUser',
+                method: "put",
+                data: user,
+                success: () => {
+                    $.getJSON("/admin/users/", function(jsondata) {
+                        console.log(jsondata);
+
+                    }.bind(this));
+                    console.log("email changed");
+                    
+
+                },
+            })
+        },
     }
 
+    
+    
 });
+       
+         
+
+
+
+
+
+
+//Notes 
+// UPDATE user SET password = getelementbydocument WHERE userID =  (this.loggedin.id)
