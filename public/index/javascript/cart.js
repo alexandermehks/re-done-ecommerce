9879e9c8e3e7bddacc82331e941b2f2942ed6f8e
rockfarmor <johan.nilsson1227@gmail.com>
2022-03-10 11:09:09 +0100

    FAKK OFF KLARNA

--- public/index/javascript/cart.js
+++ public/index/javascript/cart.js
@@ -3,7 +3,8 @@ const checkoutvm = new Vue({
     el: "#appcheckout",
     data: {
         loggedin: {},
-        
+        test_html: "",
+        klarna_obj: {}
     },
     mounted() {
         $("#navbar").load("navbar.html");
@@ -11,9 +12,9 @@ const checkoutvm = new Vue({
         this.getLoggedInUser();
     },
     methods: {
-     
+
         beforeMount() {
-          
+
             this.getLoggedInUser();
         },
 
@@ -23,9 +24,9 @@ const checkoutvm = new Vue({
                 type: 'GET',
                 success: (result) => {
                     this.loggedin = result;
-                    
-                   
-                    
+
+
+
 
 
                 }
@@ -37,9 +38,9 @@ const checkoutvm = new Vue({
                 "prodID": id
             }
             $.ajax({
-                url:'/auth//removeFromShoppingCart',
-                type:'POST',
-                data: obj, 
+                url: '/auth//removeFromShoppingCart',
+                type: 'POST',
+                data: obj,
                 success: (result) => {
                     this.getLoggedInUser()
                     navbarvm.getLoggedInUser()
@@ -51,16 +52,16 @@ const checkoutvm = new Vue({
 
 
 
-        
+
 
 
         updateAmount(value, propID) {
-           let obj = {
+            let obj = {
                 "value": value,
-                "propID":propID
-           }
+                "propID": propID
+            }
             $.ajax({
-                url:'/auth/updateAmount',
+                url: '/auth/updateAmount',
                 type: 'POST',
                 data: obj,
                 success: (result) => {
@@ -72,52 +73,66 @@ const checkoutvm = new Vue({
 
 
         //SAMPLE EMAIL FUNCTION
-        email(){
+        email() {
             $.ajax({
-                url:'/user/getEmails',
+                url: '/user/getEmails',
                 type: 'GET',
                 success: (result) => {
                     let obj = {}
                     let keys = Object.keys(result);
-                    for(let i = 0; i < keys.length; i++){
+                    for (let i = 0; i < keys.length; i++) {
                         obj[i] = result[i]
                     }
 
                     $.ajax({
-                        url:'/services/email',
+                        url: '/services/email',
                         type: 'POST',
                         data: obj,
-                        success: (email_res) =>{
+                        success: (email_res) => {
                             console.log(email_res)
+
                         }
 
                     })
                 }
             })
 
-           
+
         },
 
 
 
 
-        makeKlarnaOrder(){
+        makeKlarnaOrder() {
             let obj = {
                 "shoppingcart": this.loggedin.shoppingcart,
                 "total": this.loggedin.totalInCart
             }
-            
+
             $.ajax({
                 url: '/klarna/generateKlarnaOrderId',
                 type: 'POST',
                 data: obj,
                 success: (result) => {
                     console.log(result)
+                    this.klarna_obj = result
+
+                    var checkoutContainer = document.getElementById('my-checkout-container')
+                    checkoutContainer.innerHTML = this.klarna_obj.html_snippet;
+                    var scriptsTags = checkoutContainer.getElementsByTagName('script')
+                    for (var i = 0; i < scriptsTags.length; i++) {
+                        var parentNode = scriptsTags[i].parentNode
+                        var newScriptTag = document.createElement('script')
+                        newScriptTag.type = 'text/javascript'
+                        newScriptTag.text = scriptsTags[i].text
+                        parentNode.removeChild(scriptsTags[i])
+                        parentNode.appendChild(newScriptTag)
+                    }
+
                 }
 
             })
         }
-        
+
     }
 });
-
